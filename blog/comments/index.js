d827b5b4acf9cba37f8ee0192c74db78182304b2
BrooksPoltl <bpoltl1@gmail.com>
2020-05-07 18:12:59 -0500

    react

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -1,30 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
-
-
 app.get('/posts/:id/comments', (req, res) => {
-    res.send(commentsByPostId[req.params.id]);
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
 app.post('/posts/:id/comments', (req, res) => {
-    const { id }  = req.params;
-    const {content } = req.body;
-    const commentId = randomBytes(4).toString('hex');
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
 
-    const comments = commentsByPostId[id] || [];
-    comments.push({ id: commentId, content});
-    commentsByPostId[id] = comments;
+  const comments = commentsByPostId[req.params.id] || [];
 
-    res.status(201).send(commentsByPostId);
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
 });
 
 app.listen(4002, () => {
-    console.log('listening on port 4002');
+  console.log('Listening on 4002');
 });
