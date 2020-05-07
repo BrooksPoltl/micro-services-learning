5633c2455e2996e3aed3b81d785f6daf0c9b5c7e
BrooksPoltl <bpoltl1@gmail.com>
2020-05-07 18:02:58 -0500

    change port

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -23,6 +23,6 @@ app.post('/posts', (req, res) => {
     res.status(201).send(posts[id]);
 });
 
-app.listen(4000, () => {
-    console.log('listening on port 4000');
+app.listen(4001, () => {
+    console.log('listening on port 4001');
 });
