f796832ef618172ab2c304bc54cca7cf325b7d20
BrooksPoltl <bpoltl1@gmail.com>
2020-05-09 11:55:38 -0500

    setup skaffold

--- blog/query/index.js
+++ blog/query/index.js
@@ -51,6 +51,7 @@ app.post('/events', (req, res) => {
 app.listen(4002, async () => {
   console.log('Listening on 4002');
 
+  
   const res = await axios.get('http://event-bus-srv:4005/events');
 
   for (let event of res.data) {
