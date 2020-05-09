78e6c907771d98d7766778ff34e47b685c18176e
BrooksPoltl <bpoltl1@gmail.com>
2020-05-09 16:13:07 -0500

    error handling

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+
+
 
 const router = express.Router();
 
@@ -13,8 +17,14 @@ router.post('/api/users/signup', [
     .withMessage('Password must be between 4 and 20 characters')
 ],
 (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    throw new RequestValidationError(errors.array());
+  }
   const { email, password } = req.body;
-  
+  throw new DatabaseConnectionError();
+  console.log('Creating a user...');
+  res.send({});
 });
 
 export { router as signupRouter };
