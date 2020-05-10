352741fdb2050b558a284d92ed4304f7aec822b1
BrooksPoltl <bpoltl1@gmail.com>
2020-05-10 08:51:06 -0500

    jwt

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
-import { param } from 'express-validator';
-
+import { Password } from '../services/password';
 interface UserAttrs {
   email: string;
   password: string;
@@ -24,6 +23,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   }
+});
+
+userSchema.pre('save', async function(done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
