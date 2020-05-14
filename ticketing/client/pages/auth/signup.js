236ed28ee945a434bd7d9c2a8f4b0b2dfa1d5564
Brooks Poltl <bpoltl1@gmail.com>
2020-05-13 21:39:18 -0500

    use request hook

--- ticketing/client/pages/auth/signup.js
+++ ticketing/client/pages/auth/signup.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
+import useRequest from '../../hooks/use-request';
 
 export default () => {
   const [email, setEmail] = useState('');
@@ -42,14 +43,7 @@ export default () => {
         />
       </div>
       {errors.length > 0 && (
-        <div className="alert alert-danger">
-          <h4>Ooops....</h4>
-          <ul className="my-0">
-            {errors.map(err => (
-              <li key={err.message}>{err.message}</li>
-            ))}
-          </ul>
-        </div>
+        
       )}
       <button className="btn btn-primary">Sign Up</button>
     </form>
