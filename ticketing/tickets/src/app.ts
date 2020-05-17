859d7dd5bf95e61d3978f2d9f977c55dca37ffae
Brooks Poltl <bpoltl1@gmail.com>
2020-05-16 19:15:25 -0500

    update package

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -5,7 +5,7 @@ import cookieSession from "cookie-session";
 
 import { errorHandler, NotFoundError, currentUser } from "@bptickets/common";
 import { createTicketRouter } from "./routes/new";
-
+import { showTicketRouter } from "./routes/show";
 const app = express();
 app.set("trust proxy", true);
 app.use(json());
@@ -19,6 +19,7 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
