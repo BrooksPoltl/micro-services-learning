5b3331b481a6ef67a6c1afc707387e180d9dd988
Brooks Poltl <bpoltl1@gmail.com>
2020-05-16 21:09:32 -0500

    more test for put

--- ticketing/tickets/src/routes/__test__/update.test.ts
+++ ticketing/tickets/src/routes/__test__/update.test.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 it("returns 404 if the privided id does not exist ", async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
   await request(app)
-    .put(`/api/tickets${id}`)
+    .put(`/api/tickets/${id}`)
     .set("Cookie", global.signin())
     .send({
       title: "adsasdads",
@@ -15,13 +15,30 @@ it("returns 404 if the privided id does not exist ", async () => {
 it("returns 401 if the user is not authenticated", async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
   await request(app)
-    .put(`/api/tickets${id}`)
+    .put(`/api/tickets/${id}`)
     .send({
       title: "adsasdads",
       price: 20,
     })
     .expect(401);
 });
-it("returns 401 if the user does not own ticket", async () => {});
+it("returns 401 if the user does not own ticket", async () => {
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "adsasdads",
+      price: 20,
+    })
+    .expect(201);
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", global.signin())
+    .send({
+      title: "asas",
+      price: 1000,
+    })
+    .expect(401);
+});
 it("returns 400 if  the user provides invalid title or price", async () => {});
 it("updates the ticket provided valid inputs", async () => {});
