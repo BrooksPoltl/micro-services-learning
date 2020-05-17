9c42f39a16e80bb49d815dcea0944c709978ec56
Brooks Poltl <bpoltl1@gmail.com>
2020-05-16 20:55:10 -0500

    put router

---
+++ ticketing/tickets/src/routes/__test__/update.test.ts
@@ -0,0 +1,27 @@
+import request from "supertest";
+import { app } from "../../app";
+import mongoose from "mongoose";
+it("returns 404 if the privided id does not exist ", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+  await request(app)
+    .put(`/api/tickets${id}`)
+    .set("Cookie", global.signin())
+    .send({
+      title: "adsasdads",
+      price: 20,
+    })
+    .expect(404);
+});
+it("returns 401 if the user is not authenticated", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+  await request(app)
+    .put(`/api/tickets${id}`)
+    .send({
+      title: "adsasdads",
+      price: 20,
+    })
+    .expect(401);
+});
+it("returns 401 if the user does not own ticket", async () => {});
+it("returns 400 if  the user provides invalid title or price", async () => {});
+it("updates the ticket provided valid inputs", async () => {});
