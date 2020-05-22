78b9a102cf6ed03e47d8e88b2d9fd8e09508f850
Brooks Poltl <bpoltl1@gmail.com>
2020-05-21 20:15:00 -0500

    common events

--- ticketing/nats-test/src/publisher.ts
+++ ticketing/nats-test/src/publisher.ts
@@ -5,20 +5,10 @@ const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
 });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.log("Publisher connected to NATS");
-
-  // const data = JSON.stringify({
-  //   id: "123",
-  //   title: "concert",
-  //   price: 20,
-  // });
-
-  // stan.publish("ticket:created", data, () => {
-  //   console.log("Event published");
-  // });
   const publisher = new TicketCreatedPublisher(stan);
-  publisher.publish({
+  await publisher.publish({
     id: "123",
     title: "concert",
     price: 2,
