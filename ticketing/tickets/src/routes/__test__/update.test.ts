351551f0828bc19c8914a6af42e46d08cda86ba2
Brooks Poltl <bpoltl1@gmail.com>
2020-08-15 17:33:08 -0500

    nats env

--- ticketing/tickets/src/routes/__test__/update.test.ts
+++ ticketing/tickets/src/routes/__test__/update.test.ts
@@ -1,7 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
 import mongoose from "mongoose";
-
+import { natsWrapper } from "../../nats-wrapper";
 it("returns 404 if the privided id does not exist ", async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
   await request(app)
@@ -92,4 +92,25 @@ it("updates the ticket provided valid inputs", async () => {
     .send();
   expect(ticketResponse.body.title).toEqual("ewqeqwe");
   expect(ticketResponse.body.price).toEqual(40);
+});
+
+it("publishes an event", async () => {
+  const cookie = global.signin();
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "adsasdads",
+      price: 20,
+    })
+    .expect(201);
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "ewqeqwe",
+      price: 40,
+    })
+    .expect(200);
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
