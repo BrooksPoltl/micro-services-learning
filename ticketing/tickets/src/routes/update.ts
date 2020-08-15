e76cfbce08d077f1371a9431989ee993ff009f37
Brooks Poltl <bpoltl1@gmail.com>
2020-08-15 16:13:54 -0500

    update

--- ticketing/tickets/src/routes/update.ts
+++ ticketing/tickets/src/routes/update.ts
@@ -8,7 +8,8 @@ import {
 } from "@bptickets/common";
 
 import { Ticket } from "../models/ticket";
-
+import { TicketUpdatedPublisher } from "../events/publishers/ticket-updated-publisher";
+import { natsWrapper } from "../nats-wrapper";
 const router = express.Router();
 
 router.put(
@@ -34,6 +35,12 @@ router.put(
       price: req.body.price,
     });
     await ticket.save();
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
     res.send(ticket);
   }
 );
