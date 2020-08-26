3fbbe70c2a0f3428873f2ca3b1ea84a2c6b0645d
Brooks Poltl <bpoltl1@gmail.com>
2020-08-26 07:20:49 -0500

    ticket updated

--- ticketing/orders/src/events/listeners/ticket-created-listener.ts
+++ ticketing/orders/src/events/listeners/ticket-created-listener.ts
@@ -7,10 +7,11 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = queueGroupName;
   async onMessage(data: TicketCreatedEvent["data"], msg: Message) {
-    const { title, price } = data;
+    const { title, price, id } = data;
     const ticket = Ticket.build({
       title,
       price,
+      id,
     });
     await ticket.save();
     msg.ack();
